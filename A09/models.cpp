becc0694569a6ef0611a4ccc2b4ed5654e18d9f7
Patatone <34103646+Patatone@users.noreply.github.com>
2022-08-11 15:37:55 +0200

    A09 done

--- A09/models.cpp
+++ A09/models.cpp
@@ -55,6 +55,12 @@ M1_vertices[23] = 1.0;
 M1_indices.resize(3 * 12);
 M1_indices = { 0,1,2, 2,3,0, 0,3,4, 4,3,7, 5,6,7, 7,4,5, 1,6,5, 1,2,6, 7,6,2, 3,2,7, 4,5,0, 1,0,5 };
 
+
+
+
+
+
+
 //// M2 : Cylinder
 // Replace the code below, that creates a simple rotated square, with the one to create a cylinder.
 
@@ -76,8 +82,11 @@ M2_vertices[1]  = center_y + height;
 M2_vertices[2]  = center_z;
 //Upper circle
 for (int i = 0; i < NSlices; i++) {
+	//x for the vertex
 	M2_vertices[(i + 1) * 3 + 0] = center_x + radius * cos((float)i / NSlices * 2.0 * M_PI);
+	//y for the vertex
 	M2_vertices[(i + 1) * 3 + 1] = center_y + height;
+	//z for the vertex
 	M2_vertices[(i + 1) * 3 + 2] = center_z + radius * sin((float)i / NSlices * 2.0 * M_PI);
 }
 
@@ -86,35 +95,50 @@ M2_vertices[(NSlices + 1) * 3 + 0] = center_x;
 M2_vertices[(NSlices + 1) * 3 + 1] = center_y - height;
 M2_vertices[(NSlices + 1) * 3 + 2] = center_z;
 //Lower circle
-for (int i =(NSlices + 1); i < 2*NSlices; i++) {
+for (int i =(NSlices + 1); i < 2*NSlices+1; i++) {
+	//x for the vertex
 	M2_vertices[(i + 1) * 3 + 0] = center_x + radius * cos((float)(i - NSlices - 1) / NSlices * 2.0 * M_PI);
+	//y for the vertex
 	M2_vertices[(i + 1) * 3 + 1] = center_y - height;
+	//z for the vertex
 	M2_vertices[(i + 1) * 3 + 2] = center_z + radius * sin((float)(i - NSlices - 1) / NSlices * 2.0 * M_PI);
 }
 
 // Resizes the indices array. Repalce the values with the correct number of
 // indices (3 * number of triangles)
-M2_indices.resize(3 * NSlices * 2);
+M2_indices.resize(3 * NSlices * 4);
 
 // Indices to connect Upper circle vertices to circle center
 for (int i = 0; i < NSlices; i++) {
 	M2_indices[i * 3 + 0] = 0;
 	M2_indices[i * 3 + 1] = i + 1;
-	// With "i + 2" when we arrive at the last vertex it will go one above
+	// With "i + 2" when we arrive at the last vertex it will go one above, whit this formula at the last vortex we came back to "1"
 	// So we used "(i + 1) % NSlices + 1"
 	M2_indices[i * 3 + 2] = (i + 1) % NSlices + 1;
 }
 
 // Indices to connect Lower circle vertices to circle center
-for (int i = NSlices; i < 2 * NSlices; i++) {
+for (int i = NSlices; i < 2*NSlices; i++) {
 	M2_indices[i * 3 + 0] = NSlices + 1;
 	M2_indices[i * 3 + 1] = i + 2;
-	// With "i + 2" when we arrive at the last vertex it will go one above
-	// So we used "(i + 1) % NSlices + 1"
+	// With "i + 3" when we arrive at the last vertex it will go one above
+	// So we used "((i + 1) % NSlices) + NSlices + 2"
 	M2_indices[i * 3 + 2] = ((i + 1) % NSlices) + NSlices + 2;
 }
 
+// Lateral Cylinder part, Half triangle for each face
+for (int i = 0; i < NSlices; i++) {
+	M2_indices[(2 * NSlices + i) * 3 + 0] = i + 1;
+	M2_indices[(2 * NSlices + i) * 3 + 1] = (i + 1) % NSlices + 1;
+	M2_indices[(2 * NSlices + i) * 3 + 2] = NSlices + i + 2;
+}
 
+// Lateral Cylinder part, the missing half triangle for each face
+for (int i = 0; i < NSlices; i++) {
+	M2_indices[(3 * NSlices + i) * 3 + 0] = NSlices + i + 2;
+	M2_indices[(3 * NSlices + i) * 3 + 1] = ((i + 1) % NSlices) + NSlices + 2;
+	M2_indices[(3 * NSlices + i) * 3 + 2] = (i + 1) % NSlices + 1;
+}
 
 
 
@@ -125,30 +149,70 @@ for (int i = NSlices; i < 2 * NSlices; i++) {
 //// M3 : Sphere
 // Replace the code below, that creates a simple triangle, with the one to create a sphere.
 
+float xCircle, yCircle, zCircle, xyCircle;
+float radiusCircle = 2.0f;
+float stackCount = 48.0f; 
+float sectorCount = 144.0f;
+float sectorStep = 2 * M_PI / sectorCount;
+float stackStep = M_PI / stackCount;
+float sectorAngle, stackAngle;
+int valueOfArrayCircle = 0;
+int valueOfSecondArrayCircle = 0;
+int k1Circle, k2Circle;
+
 // Resizes the vertices array. Repalce the values with the correct number of
 // vertices components (3 * number of vertices)
-M3_vertices.resize(9);
+M3_vertices.resize(3 * stackCount * sectorCount * 2);
 
 // Vertices definitions
-M3_vertices[0]  =  0.0;
-M3_vertices[1]  =  1.0;
-M3_vertices[2]  = -1.2;
-M3_vertices[3]  = -0.866;
-M3_vertices[4]  = -0.5;
-M3_vertices[5]  = -1.2;
-M3_vertices[6]  =  0.866;
-M3_vertices[7]  = -0.5;
-M3_vertices[8]  = -1.2;
+for (int i = 0; i <= stackCount; i++) {
+	stackAngle = M_PI / 2 - i * stackStep;
+	xyCircle = radiusCircle * cos(stackAngle);
+	zCircle = radiusCircle * sin(stackAngle);
+
+	for (int j = 0; j <= sectorCount; j++) {
+		sectorAngle = j * sectorStep;
+
+		xCircle = xyCircle * cos(sectorAngle);
+		yCircle = xyCircle * sin(sectorAngle);
+		M3_vertices[(valueOfArrayCircle * 3) + 0] = xCircle;
+		M3_vertices[(valueOfArrayCircle * 3) + 1] = yCircle;
+		M3_vertices[(valueOfArrayCircle * 3) + 2] = zCircle;
+		valueOfArrayCircle++;
+	}
+}
 
 
 // Resizes the indices array. Repalce the values with the correct number of
 // indices (3 * number of triangles)
-M3_indices.resize(3);
+M3_indices.resize(3 * stackCount * sectorCount * 2);
 
 // indices definitions
-M3_indices[0] = 0;
-M3_indices[1] = 1;
-M3_indices[2] = 2;
+for (int i = 0; i < stackCount; i++) {
+	k1Circle = i * (sectorCount + 1);
+	k2Circle = k1Circle + sectorCount + 1;
+
+	for (int j = 0; j < sectorCount; j++, k1Circle++, k2Circle++) {
+		if (i != 0) {
+			M3_indices[valueOfSecondArrayCircle] = k1Circle;
+			valueOfSecondArrayCircle++;
+			M3_indices[valueOfSecondArrayCircle] = k2Circle;
+			valueOfSecondArrayCircle++;
+			M3_indices[valueOfSecondArrayCircle] = k1Circle + 1;
+			valueOfSecondArrayCircle++;
+		}
+
+		if (i != stackCount - 1) {
+			M3_indices[valueOfSecondArrayCircle] = k1Circle + 1;
+			valueOfSecondArrayCircle++;
+			M3_indices[valueOfSecondArrayCircle] = k2Circle;
+			valueOfSecondArrayCircle++;
+			M3_indices[valueOfSecondArrayCircle] = k2Circle + 1;
+			valueOfSecondArrayCircle++;
+		}
+	}
+}
+
 
 
 
@@ -160,56 +224,58 @@ M3_indices[2] = 2;
 
 //// M4 : Spring
 // Replace the code below, that creates a simple octahedron, with the one to create a spring.
-M4_vertices.resize(3 * 6);
+
+
+int nSSlices = 36;
+float steps = 100;
+float r = 0.5; //cylinder radius
+float R = 4; //distance from the center of cylinder from center of the spring
+float t = 0.f;
+int n = 4; // number of rounds
+float d = 2; //distance between rounds
+
+float sCx = 0, sCy = 0, sCz = 0;
+
+// vertices components (3 * number of vertices)
+M4_vertices.resize(3 * (steps * nSSlices));
 
 // Vertices definitions
-M4_vertices[0]  =  0.0;
-M4_vertices[1]  =  1.414;
-M4_vertices[2]  = -1.0;
-M4_vertices[3]  =  0.0;
-M4_vertices[4]  = -1.414;
-M4_vertices[5]  = -1.0;
-M4_vertices[6]  = -1.0;
-M4_vertices[7]  =  0.0;
-M4_vertices[8]  = -2.0;
-M4_vertices[9]  = -1.0;
-M4_vertices[10] =  0.0;
-M4_vertices[11] =  0.0;
-M4_vertices[12] =  1.0;
-M4_vertices[13] =  0.0;
-M4_vertices[14] =  0.0;
-M4_vertices[15] =  1.0;
-M4_vertices[16] =  0.0;
-M4_vertices[17] = -2.0;
+for (int i = 0; i < steps; i++) {
+	t = (float)i / steps * n * 2.0 * M_PI;
+	//center of i circle 
+	sCx = R * cos(t);
+	sCy = (d * t) / M_PI;
+	sCz = R * sin(t);
+	for (int j = 0; j < nSSlices; j++) {
+		M4_vertices[j * 3 + i * nSSlices * 3 + 0] = sCx + r * cos((float)j / nSSlices * 2.0 * M_PI) * cos(t); //x for the vertex
+		M4_vertices[j * 3 + i * nSSlices * 3 + 1] = sCy + r * sin((float)j / nSSlices * 2.0 * M_PI); //y for the vertex
+		M4_vertices[j * 3 + i * nSSlices * 3 + 2] = sCz + r * cos((float)j / nSSlices * 2.0 * M_PI) * sin(t); // z for the vertex
+	}
+}
 
 
 // Resizes the indices array. Repalce the values with the correct number of
 // indices (3 * number of triangles)
-M4_indices.resize(3 * 8);
+M4_indices.resize(3 * (steps * nSSlices) * 2);
 
 // indices definitions
-M4_indices[0]  = 0;
-M4_indices[1]  = 2;
-M4_indices[2]  = 3;
-M4_indices[3]  = 1;
-M4_indices[4]  = 3;
-M4_indices[5]  = 2;
-M4_indices[6]  = 0;
-M4_indices[7]  = 3;
-M4_indices[8]  = 4;
-M4_indices[9]  = 1;
-M4_indices[10] = 4;
-M4_indices[11] = 3;
-M4_indices[12] = 0;
-M4_indices[13] = 4;
-M4_indices[14] = 5;
-M4_indices[15] = 1;
-M4_indices[16] = 5;
-M4_indices[17] = 4;
-M4_indices[18] = 0;
-M4_indices[19] = 5;
-M4_indices[20] = 2;
-M4_indices[21] = 1;
-M4_indices[22] = 2;
-M4_indices[23] = 5;
+int lastVal = 0;
+
+for (int i = 0; i < steps - 1; i++) {
+	for (int j = 0; j < nSSlices; j++) {
+		M4_indices[j * 3 + i * nSSlices * 3 + 0] = j + i * nSSlices;
+		M4_indices[j * 3 + i * nSSlices * 3 + 1] = j + i * nSSlices + nSSlices;
+		M4_indices[j * 3 + i * nSSlices * 3 + 2] = ((j + 1) % nSSlices) + nSSlices * i;
+	}
+	lastVal = i * nSSlices * 3;
+}
+
+for (int i = 0; i < steps - 1; i++) {
+	for (int j = 0; j < nSSlices; j++) {
+		M4_indices[j * 3 + (steps + i) * nSSlices * 3 + 0] = j + i * nSSlices + nSSlices;
+		M4_indices[j * 3 + (steps + i) * nSSlices * 3 + 1] = ((j + 1) % nSSlices) + nSSlices * i + nSSlices;
+		M4_indices[j * 3 + (steps + i) * nSSlices * 3 + 2] = ((j + 1) % nSSlices) + nSSlices * i;
+	}
+}
+
 }
